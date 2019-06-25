b2bc2e7346bb23929009796348c14252ab6f45ae
Eric Miller <emiller@onica.com>
2019-06-24 20:54:55 -0400

    add lab 2 source

---
+++ lab_2/src/preset-simple-corridor.py
@@ -0,0 +1,48 @@
+from rl_coach.agents.clipped_ppo_agent import ClippedPPOAgentParameters
+from rl_coach.environments.gym_environment import GymVectorEnvironment
+from rl_coach.graph_managers.basic_rl_graph_manager import BasicRLGraphManager
+from rl_coach.graph_managers.graph_manager import SimpleSchedule
+from rl_coach.core_types import *
+#from rl_coach.environments.environment import SelectedPhaseOnlyDumpMethod, MaxDumpMethod
+from rl_coach import logger
+from rl_coach.base_parameters import TaskParameters
+from rl_coach.base_parameters import VisualizationParameters
+from rl_coach.architectures.embedder_parameters import InputEmbedderParameters
+
+################
+#  Environment #
+################
+
+env_params = GymVectorEnvironment(level='simple_corridor_env:SimpleCorridor')
+
+#########
+# Agent #
+#########
+
+agent_params = ClippedPPOAgentParameters()
+
+#################
+# Visualization #
+#################
+
+#env_params.frame_skip = 5 #to make sure the gifs work without skipping steps
+
+vis_params = VisualizationParameters()
+vis_params.dump_gifs=False
+
+####################
+# Graph Scheduling #
+####################
+
+schedule_params=SimpleSchedule()
+schedule_params.improve_steps = TrainingSteps(100000)
+schedule_params.steps_between_evaluation_periods = EnvironmentEpisodes(20)
+schedule_params.evaluation_steps = EnvironmentEpisodes(5)
+
+
+graph_manager = BasicRLGraphManager(
+    agent_params= agent_params,
+    env_params=env_params,
+    schedule_params=schedule_params,
+    vis_params=vis_params
+    )
---
+++ lab_2/src/simple_corridor_env.py
@@ -0,0 +1,41 @@
+import numpy as np
+import gym
+from gym.spaces import Discrete, Box
+
+class SimpleCorridor(gym.Env):
+    """Example of a custom env in which you have to walk down a corridor.
+    You can configure the length of the corridor via the env config."""
+
+    def __init__(self, config={'corridor_length': 5}):
+        self.end_pos = config["corridor_length"]
+        self.cur_pos = 0
+        self.action_space = Discrete(2)
+        self.observation_space = Box(
+            0.0, self.end_pos, shape=(1, ), dtype=np.float32)
+
+    def reset(self):
+        self.cur_pos = 0
+        self.steps = 0
+        return [self.cur_pos]
+
+    def step(self, action):
+        self.steps += 1
+        assert action in [0, 1], action
+        if action == 0 and self.cur_pos > 0:
+            self.cur_pos -= 1
+        elif action == 1:
+            self.cur_pos += 1
+        done = self.cur_pos >= self.end_pos
+        if done: #reached end of corridor
+            reward = 1
+        else:
+            reward = 0
+            
+        if self.steps >= self.end_pos*10: #timeout
+            done = True
+            reward = -1
+        observation = [self.cur_pos]
+        return observation, reward, done, {}
+    
+    def render(self, mode):
+        pass
